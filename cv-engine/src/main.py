6a48ae7a231ff8c946c9178a0827e8b8cab6f647
Morgan Yeh <fs.morganyeh@gmail.com>
2026-08-15 12:13:11 -0400

    add state machine

--- cv-engine/src/main.py
+++ cv-engine/src/main.py
@@ -11,10 +11,18 @@ from numericvision import detect_box_sequences
 import easyocr
 import threading
 from dotenv import load_dotenv
+import time
+from enum import Enum
 
 # --------------------
 # CLASSES
 # --------------------
+
+class State(Enum): # Used for state machine
+    SEARCHING = 1
+    STABILIZING = 2
+    CAPTURING = 3
+    COOLDOWN = 4
 
 class AsyncOCR:
     def __init__(self, languages=["en"], use_gpu=False):
@@ -81,6 +89,7 @@ class AsyncOCR:
 
 load_dotenv("../../.env")
 
+# YOLO classes
 TIMER_CLASS = "timer"
 CARD_CLASS = "judge_card"
 
@@ -99,8 +108,17 @@ options = vision.HandLandmarkerOptions(
 )
 detector = vision.HandLandmarker.create_from_options(options)
 
+# Roboflow Config
 ROBOFLOW_API_KEY = os.getenv("ROBOFLOW_API_KEY")
 ROBOFLOW_URL = os.getenv("ROBOFLOW_URL")
+
+# State Machine Variables
+STABILIZATION_TIME = 1.0
+COOLDOWN_TIME = 1.0
+current_state = State.SEARCHING
+detection_start_time = 0.0
+cooldown_start_time = 0.0
+current_penalty = None
 
 # --------------------
 # PENALTY DETECTION
@@ -308,10 +326,67 @@ def read_time(timer_roi):
     return None
 
 # --------------------
+# STATE MACHINE
+# --------------------
+
+def process_state_machine(detected_penalty, timer_roi):
+    """
+    Evaluates detected penalty and transitions state machine
+    """
+    global current_state, detection_start_time, cooldown_start_time, current_penalty
+    timer_result = None
+
+    # SEARCHING
+    if current_state == State.SEARCHING:
+        if detected_penalty is not None:
+            # lock on to current frame, start timer
+            current_penalty = detected_penalty
+            detection_start_time = time.time()
+
+            # transition to next state
+            current_state = State.STABILIZING
+
+    # STABILIZING
+    elif current_state == State.STABILIZING:
+        # ensure penalty did not change
+        if detected_penalty != current_penalty:
+            current_state = State.SEARCHING
+            detected_penalty = None
+        else:
+            # transition to next state if enough time has passed
+            elapsed_time = time.time() - detection_start_time
+            if elapsed_time >= STABILIZATION_TIME:
+                current_state = State.CAPTURING
+
+    # CAPTURING
+    elif current_state == State.CAPTURING:
+        # get time from timer display
+        timer_result = read_time(timer_roi)
+
+        # Transition to Cooldown
+        cooldown_start_time = time.time()
+        current_state = State.COOLDOWN 
+
+    # COOLDOWN
+    elif current_state == State.COOLDOWN:
+        # transition back to Searching if enough time has passed
+        elapsed_time = time.time() - cooldown_start_time
+        if elapsed_time >= COOLDOWN_TIME:
+            current_state = State.SEARCHING
+            current_penalty = None
+
+    return current_state, timer_result
+
+# --------------------
 # MAIN
 # --------------------
 
+current_time_str = None
+current_penalty_str = None
+
 def main():
+    global current_time_str, current_penalty_str
+
     # Load custom YOLO modelq
     model = YOLO('../models/best.pt')
 
@@ -346,16 +421,24 @@ def main():
         # determine penalty for solve
         penalty = detect_penalty(frame, card_roi)
 
-        # read timer
-        time = read_time(timer_roi)
+        # transition the state machine; get current state and time
+        current_state, time_str = process_state_machine(penalty, timer_roi)
+
+        if time_str is not None:
+            current_time_str = time_str
+        if penalty is not None and current_state == State.COOLDOWN:
+            current_penalty_str = penalty
 
         # display penalty and time on screen
-        penalty_text = f"Penalty: {penalty}" if penalty else "Searching..."
-        timer_text = f"Time: {time}"
-        cv2.putText(frame, f"Status: {timer_text}", (20, 40), 
+        penalty_text = f"Penalty: {current_penalty_str}"
+        timer_text = f"Time: {current_time_str}"
+        state_text = f"State: {current_state}"
+        cv2.putText(frame, f"{timer_text}", (20, 40), 
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.putText(frame, f"Status: {penalty_text}", (20, 80), 
+        cv2.putText(frame, f"{penalty_text}", (20, 80), 
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(frame, f"{state_text}", (20, 200), 
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 
         # Show the live feed 
