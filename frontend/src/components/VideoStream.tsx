9cfe66629e7048e4ae4e7eddcf5061dc48cdcf64
Morgan Yeh <fs.morganyeh@gmail.com>
2026-08-22 14:03:23 -0400

    add result text

--- frontend/src/components/VideoStream.tsx
+++ frontend/src/components/VideoStream.tsx
@@ -3,13 +3,20 @@ import Dropdown from './Dropdown';
 
 export default function VideoStream() {
     const options = ["Firstname Lastname", "Lastname Firstname"]
-    const resultText = "Time: 5.50\tPenalty: None\tFinal Result: 5.50";
+    const time = '45.67';
+    const penalty = '+2';
+    const result = '47.67';
 
     return (<>
         <div className="video-stream">
           <Dropdown name='curr-competitor' id='curr-competitor' text='Current Competitor' options={options} direction='row'></Dropdown>
           <img src="http://127.0.0.1:8000/video_feed" alt="Live Camera Feed" className="live-feed"></img>
-          <div className="result">{resultText}</div>
+          <div className="result-container">
+            <div className="result-text" id='time'>Time: {time}</div>
+            <div className='result-text' id='penalty'>Penalty: {penalty}</div>
+            <div className='result-text' id='final-result'>Result: {result}</div>
+          </div>
+          
         </div>
     </>);
 };
