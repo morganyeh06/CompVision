a1bfad23ac17244477a7ca25f7cb3f30789664ba
Morgan Yeh <fs.morganyeh@gmail.com>
2026-08-23 21:39:06 -0400

    add backend endpoints

--- backend/main.py
+++ backend/main.py
@@ -6,15 +6,21 @@ from cv_engine.src.detect_penalty import detect_penalty
 from cv_engine.src.pipeline import State, process_state_machine
 from ultralytics import YOLO
 
-# --------------------
-# GLOBAL VARIABLES
-# --------------------
+from pydantic import BaseModel
+from typing import List, Optional
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+import math
+
+# --------------------------
+# GLOBAL VARIABLES & CONFIG
+# --------------------------
 
 camera = cv2.VideoCapture(0)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-# YOLO classes
+# YOLO Classes
 TIMER_CLASS = "timer"
 CARD_CLASS = "judge_card"
 YOLO_FREQ = 15 # how often to run YOLO on a frame
@@ -29,8 +35,39 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+# allow frontend server to communicate with FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Global State
+cv_state = {"time": None, "penalty": None, "state": None}
+app_settings = {}
+leaderboard_df = pd.DataFrame()
+
+
 # --------------------
-# GENERATOR
+# CLASSES
+# --------------------
+
+class CompetitionSettings(BaseModel):
+    competition_name: str
+    event: str
+    round_number: str
+    avg_format: str
+    competitors: List[str]
+
+class SaveResultRequest(BaseModel):
+    competitor_name: str
+    solve_index: int
+    final_result: str
+
+
+# --------------------
+# HELPER FUNCTIONS
 # --------------------
 
 def generate_frames():
@@ -77,16 +114,20 @@ def generate_frames():
             
             # transition the state machine; get current state and time
             current_state, time_str = process_state_machine(penalty, timer_roi)
-        
+
+            # update cv state
             if time_str is not None:
-                current_time_str = time_str
+                cv_state["time"] = time_str
             if penalty is not None and current_state == State.COOLDOWN:
-                current_penalty_str = penalty
+                cv_state["penalty"] = penalty
+
+            cv_state["state"] = str(current_state)
+            
         
         # display penalty, time, and state
-        penalty_text = f"Penalty: {current_penalty_str}"
-        timer_text = f"Time: {current_time_str}"
-        state_text = f"State: {current_state}"
+        penalty_text = f"Penalty: {cv_state['penalty']}"
+        timer_text = f"Time: {cv_state['time']}"
+        state_text = f"State: {cv_state['state']}"
         cv2.putText(frame, f"{timer_text}", (20, 40), 
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.putText(frame, f"{penalty_text}", (20, 80), 
@@ -111,6 +152,38 @@ def generate_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 
+def calculate_wca_result(raw_time_str: str, penalty: str) -> str:
+    """
+    Processes raw time string, applies penalty, then formats as WCA time
+    """
+    if not raw_time_str or penalty == "DNF":
+        return "DNF"
+
+    # calculate total number of seconds in the time
+    time_parts = raw_time_str.split(":")
+    total_seconds = 0
+    if len(time_parts == 2):
+        total_seconds = int(time_parts[0]) * 60 + float(time_parts[1])
+    else:
+        total_seconds = float(time_parts[0])
+
+    # apply +2 penalty if applicable
+    if penalty == "+2": 
+        total_seconds += 2.0
+
+    # format time as M:SS.SS
+    if total_seconds >= 60:
+        mins = int(total_seconds // 60)
+        secs = total_seconds % 60
+        return f"{mins}:{secs:05.2f}"
+    else:
+        return f"{total_seconds:.2f}"
+    
+
+# --------------------
+# ENDPOINTS
+# --------------------
+
 @app.get("/video_feed")
 def video_feed():
     """
@@ -120,3 +193,64 @@ def video_feed():
         generate_frames(),
         media_type="multipart/x-mixed-replace; boundary=frame"
     )
+
+
+@app.post("/settings")
+def save_settings(settings: CompetitionSettings):
+    """
+    Saves competition settings to global state
+    """
+    global app_settings, leaderboard_df
+    app_settings = settings.model_dump()
+
+    # generate dataframe columns based on avg format
+    num_solves = 5 if settings.avg_format.lower() == "ao5" else 3
+    columns = [f"Solve {i+1}" for i in range(num_solves)]
+
+    # create dataframe indexed by competitor names
+    leaderboard_df = pd.DataFrame(index=settings.competitors, columns=columns)
+
+    return {
+        "status": "success",
+        "message": "Settings applied and leaderboard created."
+    }
+
+
+@app.get("/latest_result")
+def get_latest_result():
+    """
+    Gets latest solve result from cv_state
+    """
+    raw_time = cv_state["time"]
+    penalty = cv_state["penalty"]
+
+    # calculate final result
+    final_result = calculate_wca_result(raw_time, penalty) if raw_time else None
+
+    return {
+        "raw_time": raw_time,
+        "penalty": penalty,
+        "final_result": final_result,
+        "cv_status": cv_state["state"]
+    }
+
+
+@app.post("/save-result")
+def save_result(req: SaveResultRequest):
+    """
+    Saves competitor's result to the leaderboard
+    """
+    global leaderboard_df
+
+    # add competitor to leaderboard if not already there
+    if req.competitor_name not in leaderboard_df.index:
+        leaderboard_df.loc[req.competitor_name] = ""
+
+    # add resilt to leaderboard
+    col_name = f"Solve {req.solve_index}"
+    leaderboard_df.at[req.competitor_name, col_name] = req.final_time
+
+    return {
+        "status": "success",
+        "leaderboard": leaderboard_df.to_dict(orient="index")
+    }
